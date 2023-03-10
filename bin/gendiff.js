8f90c2260638f7530448c006c3aa6bf182edc3a5
Goga-Rid <wopa22@list.ru>
2023-03-10 14:07:14 +0300

    adding new references

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,7 @@ const program = new Command();
 program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0', '-V, --version', 'output the version number')
-
+    .option('-f, --format, <type>', 'output format')
+    .arguments('<filepath1> <filepath2>')
+    .helpOption('-h, --help', 'output usage information')
 program.parse();
