6379fe551f0a2e8ba1f19557b4717cb9a37cc5f3
Goga-Rid <wopa22@list.ru>
2023-06-10 01:56:39 +0300

    refactoring code

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import makeDiff from './makeDiff.js';
 import getParse from './parsers.js';
 import formatDiff from './formatters/index.js';
 
-// ------------- Basic functions for converting the relative path to a file-------------
 const absPath = (filepath) => path.resolve(process.cwd(), filepath).trim();
 
 const getExtention = (filepath) => path.extname(filepath).slice(1).toLowerCase();
 
 const getData = (filepath) => fs.readFileSync(absPath(filepath), 'utf-8');
-// -------------------------------------------------------------------------------------
+
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const data1 = getParse(getData(filepath1), getExtention(filepath1));
   const data2 = getParse(getData(filepath2), getExtention(filepath2));
